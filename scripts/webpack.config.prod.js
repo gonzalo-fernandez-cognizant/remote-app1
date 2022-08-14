95a9f98e62a47a86a01d2ed4a7b97930e6c07f9c
Gonzalo Fernandez <gonzalo.fernandez@softvision.com>
2022-08-14 19:20:29 -0300

    update config

--- scripts/webpack.config.prod.js
+++ scripts/webpack.config.prod.js
@@ -12,8 +12,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.[contenthash].js',
         path:  path.resolve(__dirname, '../dist'),
-        publicPath: '',
-        // publicPath: 'file:///home/gonzalofernandez/projects/pocs-and-tutos/webpack-tuto/module-federation/build-test/',
+        publicPath: 'https://remote-app1.vercel.app/',
         // clean: true
         // clean: {
         //     dry: true
@@ -86,14 +85,14 @@ module.exports = {
             filename: '[name].[contenthash].css'
         }),
         new CleanWebpackPlugin(),
-        // new HtmlWebpackPlugin({
-        //     template: './component/index.html',
-        //     chunks: ['index'],
-        //     meta: {
-        //         description: 'Meta tag inserted from webpack'
-        //     },
-        //     filename: 'index.html'
-        // }),
+        new HtmlWebpackPlugin({
+            template: './component/index.html',
+            chunks: ['index'],
+            meta: {
+                description: 'Meta tag inserted from webpack'
+            },
+            filename: 'index.html'
+        }),
         new ModuleFederationPlugin({
             name: 'MyApp1',
             filename: 'remoteApp1.js',
